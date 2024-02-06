302eed06bdaf9a3c0c73bf60f8b256693b39fd59
sajayjoe <sajayjose24@gmail.com>
2024-02-06 12:50:55 +0530

    final commit

--- Tourism/app/models.py
+++ Tourism/app/models.py
@@ -30,16 +30,7 @@ class Package(models.Model):
     def __str__(self):
         return self.package_name + ' created by ' + self.user_id.username
     
-class HealthAssistant(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=10)
-    qualification = models.CharField(max_length=200)
-    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.first_name} ({self.qualification})"
 
 
 class Booking(models.Model):
@@ -54,7 +45,6 @@ class Booking(models.Model):
     no_of_people = models.IntegerField()
     booking_date = models.DateField()
     date = models.DateTimeField(auto_now_add=True)
-    HealthAssistant_id = models.ForeignKey(HealthAssistant, on_delete=models.CASCADE,null=True)
     status = models.CharField(choices=booking_status, default='pending', max_length=50)
     total_amount = models.IntegerField()
     rating = models.IntegerField(null=True, blank=True)
